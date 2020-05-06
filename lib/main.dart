e030f7ed5cbcd0eed87b6f0fc6ab0297275bae90
brianking13 <bkingrr13@gmail.com>
2020-05-06 01:14:28 -0700

    now includes working model of a keyboard

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,30 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  String character = "None";
+  List <IconData> characters = [];
 
-  setCharacter(input){
-    character = input;
-    return character;
+  void setCharacter(input){
+    characters.add(input);
   }
 
-  printCharacter(){
-    return character;
+  void clear(){
+    characters = [];
   }
+
+  void send(){
+    for (var i = 0; i <= characters.length-1; i++){
+      print(translate(characters[i].toString()));
+  }}
+
+  //translate from icondata to text
+  translate(x){
+    if (x == "IconData(U+0E5DB)"){return "Down";}
+    if (x == "IconData(U+0E5D8)"){return "Up";}
+    if (x == "IconData(U+0E5C4)"){return "Left";}
+    if (x == "IconData(U+0E5C8)"){return "Right";}
+    else {return "Error";}
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -33,15 +47,36 @@ class _HomeState extends State<Home> {
       Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
+
+          // First row of buttons
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               FlatButton.icon(onPressed: (){setState(() {
-                setCharacter('A');
-              });}, icon: Icon(Icons.keyboard, color: Colors.white70,), label: Text("A",style: TextStyle(color: Colors.white70),), splashColor: Colors.redAccent,),
+                setCharacter(Icons.arrow_downward);
+              });}, icon: Icon(Icons.arrow_downward, color: Colors.white70,), label: Text("Down",style: TextStyle(color: Colors.white70),), splashColor: Colors.redAccent,),
               FlatButton.icon(onPressed: (){setState(() {
-                  setCharacter('B');
-              });}, icon: Icon(Icons.keyboard, color: Colors.white70,), label: Text("B",style: TextStyle(color: Colors.white70),), splashColor: Colors.redAccent,),
+                  setCharacter(Icons.arrow_upward);
+              });}, icon: Icon(Icons.arrow_upward, color: Colors.white70,), label: Text("Up",style: TextStyle(color: Colors.white70),), splashColor: Colors.redAccent,),
+              FlatButton.icon(onPressed: (){setState(() {
+                setCharacter(Icons.arrow_back);
+              });}, icon: Icon(Icons.arrow_back, color: Colors.white70,), label: Text("Left",style: TextStyle(color: Colors.white70),), splashColor: Colors.redAccent,),
+              FlatButton.icon(onPressed: (){setState(() {
+                setCharacter(Icons.arrow_forward);
+              });}, icon: Icon(Icons.arrow_forward, color: Colors.white70,), label: Text("Right",style: TextStyle(color: Colors.white70),), splashColor: Colors.redAccent,),
+            ],
+          ),
+
+          // Second row of buttons
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton.icon(onPressed: (){setState(() {
+                clear();
+              });}, icon: Icon(Icons.clear, color: Colors.white70,), label: Text("Clear",style: TextStyle(color: Colors.white70),), splashColor: Colors.redAccent,),
+              FlatButton.icon(onPressed: (){setState(() {
+                send();
+              });}, icon: Icon(Icons.send, color: Colors.white70,), label: Text("Send",style: TextStyle(color: Colors.white70),), splashColor: Colors.redAccent,),
             ],
           ),
           SizedBox(height: 20,),
@@ -50,8 +85,19 @@ class _HomeState extends State<Home> {
             height: data.size.height * .3,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Text(printCharacter())
+                Expanded(
+                  child: Container(
+                    color: Colors.white,
+                    child: ListView.builder(scrollDirection: Axis.horizontal,itemCount: characters.length,itemBuilder:(context, index){
+                      return Padding(
+                        padding: EdgeInsets.all(2.0),
+                        child: Icon(characters[characters.length-index-1]), //input from right to left
+                      );
+                    }),
+                  ),
+                )
               ],
             ),
           ),
